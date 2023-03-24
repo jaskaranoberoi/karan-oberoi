34bfed409a962b9d162ba4241bf185fa67400457
jaskaranoberoi <128745608+jaskaranoberoi@users.noreply.github.com>
2023-03-25 01:33:47 +0530

    Create Area_of_rhombus.c

---
+++ Area_of_rhombus.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int d1,d2, Area;
+    printf("Enter first diagonal  of rhombus ");
+    scanf("%d",&d1);
+     printf("Enter second  diagonal  of rhombus ");
+    scanf("%d",&d2);
+    Area = (d1*d2)/2;
+    printf("Area of rhombus is %d",Area);
+    
+    
+return 0;    
+}
