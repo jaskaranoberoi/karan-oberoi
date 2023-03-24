4950b677ca98876845a4115a1b4005e4898943e1
jaskaranoberoi <128745608+jaskaranoberoi@users.noreply.github.com>
2023-03-25 01:15:07 +0530

    Create Upper_Case_Into_Lower_Case.c

---
+++ Upper_Case_Into_Lower_Case.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    char ch1 , ch2;
+    printf("Enter Character In Upper Case :");
+    scanf("%c",&ch1);
+    ch2=ch1+32;
+    printf("Character In Lower Case Is %d",ch2);
+return 0;    
+    
+    
+}
