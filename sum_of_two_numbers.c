c4060e01acb581cde4d95badbe7b99c8aad46e71
jaskaranoberoi <128745608+jaskaranoberoi@users.noreply.github.com>
2023-03-25 00:32:13 +0530

    Create sum_of_two_numbers.c

---
+++ sum_of_two_numbers.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int a,b,sum;
+    printf("Enter First Number:");
+    scanf("%d",&a);
+    printf("Enter Second Number:");
+    scanf("%d",&b);
+    sum=a+b;
+    printf("The sum is %d",sum);
+return 0;
+}
